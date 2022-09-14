61316b7701109da9110e3f213243ef60e58ca38d
Vaibhav <kasarvaibhav777@gmail.com>
2022-09-14 21:15:01 +0530

    14-09-2022

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -24,7 +24,35 @@ const createColleges= async function(req,res){
 
 }
 
+let collegeDetails = async function (req, res ) {
+    try {
+        if (req.query.collegeName) {
+            let college = await collegeModel.findOne({name: req.query.collegeName, isDeleted: false})
+            if(!college) {
+                res.status(404).send({status: false, msg: "college name not found"})
+            } else {
+                let collegeData = {
+                    name: college.name,
+                    fullName: college.fullName,
+                    logoLink: college.logoLink
+                }
+                let interns = await internModel.find({collegeId: college._id, isDeleted: false }, "-collegeId -isDeleted -createdAt -updatedAt -__v").sort({createdAt: -1})
+                if(interns) {
+                    collegeData.interns = interns
+                }
+                res.status(201).send({ status: true, data: collegeData})
+            }
+        } else {
+            res.status(400).send({ status: false, msg: "College name not present"})
+        }
+    }
+    catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+    }
+}
+
 module.exports={createColleges}
+module.exports.collegeDetails = collegeDetails
 
 
 
