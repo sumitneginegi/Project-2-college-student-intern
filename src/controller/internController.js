1b34be4fd7a612838cdccb6b63b37e0dc2cc5ca6
Vaibhav <kasarvaibhav777@gmail.com>
2022-09-14 20:58:38 +0530

    14-09-2022

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,30 +1,60 @@
-let collegeModel= require("../models/collegeModel")
-let internModel= require("../models/internModel")
+let collegeModel = require("../models/collegeModel")
+let internModel = require("../models/internModel")
 
 
 let createIntern = async function (req, res) {
     try {
-        res.setHeader('Access-Control-Allow-Origin','*')
-        if (req.body && Object.keys(req.body).length > 0) {
-            let college = await collegeModel.findOne({name: req.body.collegeName, isDeleted: false })
-             if(!college){
-                res.status(400).send({status: false, msg: "Request must contain a college"})
-             } else {
-                let internData = {
-                    name: req.body.name,
-                    email: req.body.email,
-                    mobile: req.body.mobile,
-                    collegeId: college._id
-                }
-                let intern = await internModel.create(internData)
-                res.status(201).send({ status: true, data: intern})
-             }
-        }else {
-            res.status(400).send({ status: false, msg: "Request not contains a body"})
+        let data = req.body
+
+        if (Object.keys(data).length == 0) {
+            res.status(200).send({ status: false, msg: "Body input is necessary" })
         }
+
+        let { name, email, mobile, collegeName } = data
+
+        if (!(name && email && mobile && collegeName)) {
+            res.status(200).send({ status: false, msg: "Mandatory fields are required" })
+        }
+
+        let clg = await collegeModel.findOne({ isDeleted: false, name: collegeName })
+        if (!clg) { res.status(404).send({ status: false, msg: "Plz put correct college Name" }) }
+
+        let obj = { ...data, collegeId: clg._id }
+
+        let guru = await internModel.create(obj)
+        res.status(201).send({ status: true, data: guru })
+
+
     } catch (error) {
         res.status(500).send({ status: false, msg: error.message })
     }
 }
 
-module.exports.createIntern = createIntern
+let getIntern = async function (req, res) {
+    try {
+        let collegeName = req.query.collegeName
+        console.log(collegeName)
+
+        let getC = await collegeModel.findOne({ name: collegeName, isDeleted: false })
+        if (!getC) { res.status(404).send({ status: false, msg: "No College Found with Given CollegeName" }) }
+
+        let data = { name: getC.name, fullName: getC.fullName, logoLink: getC.logoLink }
+
+        let intern = await internModel.find({ collegeId: getC._id, isDeleted: false })
+        if (!intern) { res.status(404).send({ status: false, msg: "No Intern Available in this College" }) }
+        else {
+            data.intern = intern
+            return res.status(200).send({ status: true, data: data })
+        }
+       
+       
+
+    }
+
+    catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+    }
+
+}
+
+module.exports = { createIntern, getIntern }
--- src/route/route.js
+++ src/route/route.js
@@ -1,14 +1,18 @@
 //=====================Importing Module and Packages=====================//
 const express = require('express');
 const router = express.Router();
+
 const {createColleges}=require("../controller/collegeController")
-const {createIntern}=require("../controller/internController")
+const {createIntern,getIntern}=require("../controller/internController")
 
 
 
 
 router.post("/functionup/colleges",createColleges)
+
 router.post("/functionup/interns",createIntern)
+
+router.post("/functionup/collegeDetails",getIntern)
 
 
 
